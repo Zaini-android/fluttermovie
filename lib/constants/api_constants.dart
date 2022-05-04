950b4910ee462375b3eae1073a6724b62aa96400
Zaini-android <74230702+Zaini-android@users.noreply.github.com>
2022-05-04 13:19:13 +0500

    Initial commit

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "c159546900f529b6788d32acb2106657";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
